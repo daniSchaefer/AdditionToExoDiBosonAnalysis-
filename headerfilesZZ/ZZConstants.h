1ad88c9eb46a91ab00289bc4d6acd66a94b40691
Daniela Schaefer <daniela.schafer@cern.ch>
2016-06-22 11:41:15 +0200

    new headerfiles for ZZ channel added

--- headerfilesZZ/ZZConstants.h
+++ headerfilesZZ/ZZConstants.h
@@ -2,7 +2,7 @@
 #define ZZCONSTANTS_H
 
 // constants for electron selection (Z)
-#define ZZELEPT 35
+#define ZZELEPT 20
 #define ZZELEPTBIG 115
 #define ZZELEETA 2.5
 #define ZZELEECALGAPMIN 1.44
@@ -12,22 +12,22 @@
 // constants for muon selection (Z)
 #define ZZMUPT 20
 #define ZZMUPTBIG 50
-#define ZZMUETA 2.4
-#define ZZMUETASMALL 2.1
+#define ZZMUETA 2.1
+#define ZZMUETASMALL 2.4
 
 //constants for jet selection
-#define ZZCA8JETMASSMIN 70
-#define ZZCA8JETMASSMAX 110
+#define ZZCA8JETMASSMIN 65
+#define ZZCA8JETMASSMAX 105
 #define ZZCA8JETMASS 40
 #define ZZCA8JETPT 200
 #define ZZCA8JETETA 2.4
 #define ZZCA8JETDELTAR 0.8
-#define ZZCA8JETTAU21HIGHPURITY 0.6
+#define ZZCA8JETTAU21HIGHPURITY 0.45
 #define ZZCA8JETTAU21LOWPURITY 0.75
 
 
 //constants for leptonic Z
-#define ZZLEPTONICZPT 200
+#define ZZLEPTONICZPT 170
 #define ZZLEPTONICZMASSMIN 70
 #define ZZLEPTONICZMASSMAX 110
 
@@ -37,7 +37,7 @@
 #define ZZHADRONICZMASSMAX 110
 
 //constants for ZZ system
-#define ZZMASSMIN 400
+#define ZZMASSMIN 600
 #define ZZMASSMAX 4800
 
 #endif
